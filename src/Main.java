ff66263150c49c9bbdf663ecc6e7cf351fc46a6a
rooshan1234 <itsrooshan@gmail.com>
2019-10-07 20:22:03 -0600

    Added more sophisticated logging to ensure the file can continue to parse with warnings

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import utils.FilenameUtils;
 
@@ -20,34 +21,41 @@ public class Main {
         HashMap<String, HashSet<LogEntry>> extensions = new HashMap<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(new File("log.json")))) {
-
             String entry = "";
-            while ((entry = reader.readLine()) != null) {
-                // deserialize
-                try {
-                    LogEntry logEntry = mapper.readValue(entry, LogEntry.class);
+            try {
+                while ((entry = reader.readLine()) != null) {
+                    try {
+                        // deserialize input line into LogEntry from file
+                        LogEntry logEntry = mapper.readValue(entry, LogEntry.class);
 
-                    if (logEntry.isValid()) {
-                        // extract the filename extension
-                        Optional<String> fileNameExtension = FilenameUtils.extractFilenameExtension(logEntry.getFilename());
+                        if (logEntry.isValid()) {
+                            // extract the filename extension
+                            Optional<String> fileNameExtension = FilenameUtils.extractFilenameExtension(logEntry.getFilename());
 
-                        // check if the filename is present
-                        if (fileNameExtension.isPresent()) {
-                            HashSet<LogEntry> files = extensions.getOrDefault(fileNameExtension.get(), new HashSet<>());
+                            // check if the filename is present
+                            if (fileNameExtension.isPresent()) {
+                                HashSet<LogEntry> files = extensions.getOrDefault(fileNameExtension.get(), new HashSet<>());
 
-                            files.add(logEntry);
-                            extensions.put(fileNameExtension.get(), files);
+                                files.add(logEntry);
+                                extensions.put(fileNameExtension.get(), files);
+                            }
                         }
+
+                    } catch (JsonMappingException | JsonParseException px) {
+                        // raised from readValue
+                        // if a single line has parsing exception, we will continue to parse the rest of the file
+                        LOGGER.log(Level.WARNING, "Was unable to parse line: " + px.getMessage());
                     }
-
-                } catch (JsonParseException px) {
-                    // if a single line has parsing exception, we will continue to parse the rest of the file
-                    LOGGER.log(Level.WARNING, "Was unable to parse line: " + px.getMessage());
                 }
-
+            } catch (IOException ex) {
+                // raised from readLine
+                // if a single line cannot be read from the buffer, we will do our best to continue
+                LOGGER.log(Level.WARNING, "Was unable to read line from buffer: " + ex.getMessage());
             }
         } catch (IOException io) {
-            io.printStackTrace();
+            // raised from BufferedReader, FileReader or File
+            // we cannot continue if we cannot initialize the buffer or the file is not found
+            throw new RuntimeException(io);
         }
 
         for (Map.Entry<String, HashSet<LogEntry>> entry : extensions.entrySet()) {
